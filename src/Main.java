57016e14d336c2cb0ca6cefac13914360981bc43
kamil <almetov.kamil@gmail.com>
2022-10-24 21:41:06 +0300

    Add A* algorithm, generation of random sample, calculation statistical characteristics (not completed yet).

--- src/Main.java
+++ src/Main.java
@@ -13,31 +13,142 @@ public class Main {
         map.generateMap();
         map.printUnits();
         //System.out.println(map.units);
-        ArrayList<Cell> res = SearchAlgorithms.backtracking(map);
-        PrintWriter printWriter = new PrintWriter("output.txt");
+        long backtrackingTime = System.currentTimeMillis();
+        ArrayList<Cell> pathBacktracking = SearchAlgorithms.backtracking(map);
+        backtrackingTime = System.currentTimeMillis() - backtrackingTime;
+        if (pathBacktracking == null) System.out.println("Backtracking = 0");
+        else System.out.println("Backtracking = " + pathBacktracking.size());
+        System.out.println(backtrackingTime + " ms");
+        long AStarTime = System.currentTimeMillis();
+        ArrayList<Cell> pathAStar = SearchAlgorithms.AStar(map);
+        AStarTime = System.currentTimeMillis() - AStarTime;
+        if (pathAStar == null) System.out.println("A* = 0");
+        else System.out.println("A* = " + pathAStar.size());
+        System.out.println(AStarTime + " ms");
+        PrintWriter printWriterBacktracking = new PrintWriter("outputBacktracking.txt");
+        PrintWriter printWriterAStar = new PrintWriter("outputAStar.txt");
 
-        if (res == null) {
-            printWriter.println("Lose");
+        if (pathBacktracking == null) {
+            printWriterBacktracking.println("Lose");
         } else {
-            printWriter.println("Win");
-            for (var cell : res) {
-                printWriter.printf("[%d,%d] ", cell.x, cell.y);
+            printWriterBacktracking.println("Win");
+            for (var cell : pathBacktracking) {
+                printWriterBacktracking.printf("[%d,%d] ", cell.x, cell.y);
             }
-            printWriter.println();
-            printWriter.println(map.mapWithPathToString(res));
-            printWriter.println("TODO - TIME");
+            printWriterBacktracking.println();
+            printWriterBacktracking.println(map.mapWithPathToString(pathBacktracking));
+            printWriterBacktracking.println("TODO - TIME");
         }
-        printWriter.close();
-        //System.out.println(res);
-        //map.print();
+        printWriterBacktracking.close();
+
+        if (pathAStar == null) {
+            printWriterAStar.println("Lose");
+        } else {
+            printWriterAStar.println("Win");
+            for (var cell : pathAStar) {
+                printWriterAStar.printf("[%d,%d] ", cell.x, cell.y);
+            }
+            printWriterAStar.println();
+            printWriterAStar.println(map.mapWithPathToString(pathAStar));
+            printWriterAStar.println("TODO - TIME");
+        }
+        printWriterAStar.close();
+
+    }
+
+
+}
+
+class SampleTest {
+    public static void main(String[] args) throws Exception {
+        Map map = new Map();
+        int wins = 0, losses = 0;
+        HashMap<Long, Integer> pdfBacktracking = new HashMap<>(), pdfAStar = new HashMap<>();
+        ArrayList<Long> sampleBacktracking = new ArrayList<>(1000), sampleAStar = new ArrayList<>(1000);
+        for (int i = 0; i < 10000; ++i) {
+            map.generateMapRandomly();
+            long backtrackingTime = System.currentTimeMillis();
+            ArrayList<Cell> pathBacktracking = SearchAlgorithms.backtracking(map);
+            backtrackingTime = System.currentTimeMillis() - backtrackingTime;
+            System.out.println(backtrackingTime);
+            int backtracking = 0;
+            int aStar = 0;
+            if (pathBacktracking != null) backtracking = pathBacktracking.size();
+            long AStarTime = System.currentTimeMillis();
+            ArrayList<Cell> pathAStar = SearchAlgorithms.AStar(map);
+            AStarTime = System.currentTimeMillis() - AStarTime;
+            if (pathAStar != null) aStar = pathAStar.size();
+            if (backtracking != aStar) {
+                map.print();
+                map.printUnits();
+                if (pathBacktracking != null) {
+                    for (var cell : pathBacktracking) {
+                        System.out.printf("[%d,%d] ", cell.x, cell.y);
+                    }
+                    System.out.println();
+                }
+                if (pathAStar != null) {
+                    for (var cell : pathAStar) {
+                        System.out.printf("[%d,%d] ", cell.x, cell.y);
+                    }
+                    System.out.println();
+                }
+                throw new Exception("ERROR");
+            }
+            map.clear();
+            sampleBacktracking.add(backtrackingTime);
+            if (!pdfBacktracking.containsKey(backtrackingTime)) {
+                pdfBacktracking.put(backtrackingTime, 0);
+            } else {
+                pdfBacktracking.put(backtrackingTime, pdfBacktracking.get(backtrackingTime)+1);
+            }
+            sampleAStar.add(AStarTime);
+            if (!pdfAStar.containsKey(AStarTime)) {
+                pdfAStar.put(AStarTime, 0);
+            } else {
+                pdfAStar.put(AStarTime, pdfAStar.get(AStarTime)+1);
+            }
+
+            if (backtracking > 0) {
+                wins++;
+            } else {
+                losses++;
+            }
+        }
+        printStatistics(wins, losses, sampleBacktracking, pdfBacktracking, "Backtracking");
+        printStatistics(wins, losses, sampleAStar, pdfAStar, "A*");
+    }
+
+    public static void printStatistics(int wins, int losses, ArrayList<Long> sample, HashMap<Long, Integer> pdf, String algorithmName) {
+        int n = wins + losses;
+        double mean = sample.stream().mapToDouble(value -> value).sum() / n;
+        long mode = pdf.entrySet().stream().max(java.util.Map.Entry.comparingByValue()).get().getKey();
+        sample.sort(null);
+        double median = (sample.get(n / 2 - 1) + sample.get(n/2))*1./2;
+        double standardDeviation = Math.sqrt(sample.stream().mapToDouble(e -> (e-mean)*(e-mean)).sum() / n);
+        //System.out.println(sample);
+        System.out.printf("""
+                %s (1 variant):
+                Mean: %.2f ms
+                Mode: %d ms
+                Median: %.2f ms
+                Standard deviation: %.2f ms
+                Number of wins: %d
+                Number of losses: %d
+                Percent of wins: %.2f %%
+                Percent of losses: %.2f %%
+               """, algorithmName, mean, mode, median, standardDeviation,
+                wins, losses, wins*1./n * 100, losses*1./n * 100);
     }
 }
 
-class Cell {
+class Cell implements Comparable<Cell> {
     int x, y;
     CellType type;
     int d = Integer.MAX_VALUE;
     Cell parent;
+    int f, g, h;
+    boolean hasVisitedTortuga, isKrakenKilled;
 
     Cell(CellType type, int x, int y) {
         this.type = type;
@@ -55,9 +166,13 @@ class Cell {
                 '}';
     }
 
-    public boolean isNonDangerous() {
-        return this.type != CellType.Kraken && this.type != CellType.DavyJones && this.type != CellType.Rock
-                && this.type != CellType.KrakenBelowRock && this.type != CellType.PerceptionZone;
+    @Override
+    public int compareTo(Cell o) {
+        if (this.f == o.f) {
+            // TODO - is it correct??
+            return this.h - o.h;
+        }
+        return this.f - o.f;
     }
 }
 
@@ -201,7 +316,7 @@ class Map {
         }
     }
 
-    private void generateMapRandomly() {
+    public void generateMapRandomly() {
         Random random = new Random();
         boolean[] used = new boolean[CellType.NUMBER_OF_UNITS];
         for (int i = 0; i < CellType.NUMBER_OF_UNITS; ++i) {
@@ -390,7 +505,26 @@ class Map {
         } else {
             kraken = units.get(CellType.KrakenBelowRock);
         }
-        return Math.abs(cell.x - kraken.x) == 1 && Math.abs(cell.y - kraken.y) == 1;
+        return isNonDangerous(cell, false)
+                && Math.abs(cell.x - kraken.x) == 1 && Math.abs(cell.y - kraken.y) == 1;
+    }
+
+    public Cell[] getKrakenWeaknesses() {
+        Cell kraken;
+        if (units.containsKey(CellType.Kraken)) {
+            kraken = units.get(CellType.Kraken);
+        } else {
+            kraken = units.get(CellType.KrakenBelowRock);
+        }
+        ArrayList<Cell> result = new ArrayList<>(4);
+        for (int i = Math.max(0, kraken.x - 1); i <= Math.min(size - 1, kraken.x + 1); ++i) {
+            for (int j = Math.max(0, kraken.y - 1); j <= Math.min(size - 1, kraken.y + 1); ++j) {
+                if (isKrakenWeakness(cells[i][j])) {
+                    result.add(cells[i][j]);
+                }
+            }
+        }
+        return result.toArray(new Cell[0]);
     }
 
     public boolean isNonDangerous(Cell cell, boolean isKrakenDead) {
@@ -469,33 +603,176 @@ class SearchAlgorithms {
     }
 
     public static ArrayList<Cell> backtracking(Map map) {
-        ArrayList<Cell> result = backtracking(map, new ArrayList<>(), map.cells[0][0],
+        ArrayList<Cell> result1 = backtracking(map, new ArrayList<>(), map.cells[0][0],
                 map.units.get(CellType.DeadManChest), 0, false, false);
         for (int i = 0; i < map.size; ++i) {
             for (int j = 0; j < map.size; ++j) {
                 map.cells[i][j].d = Integer.MAX_VALUE;
             }
         }
-        if (result == null) {
-            result = backtracking(map, new ArrayList<>(), map.cells[0][0],
-                    map.units.get(CellType.Tortuga), 0, false, false);
-            for (int i = 0; i < map.size; ++i) {
-                for (int j = 0; j < map.size; ++j) {
-                    map.cells[i][j].d = Integer.MAX_VALUE;
-                }
-            }
-            if (result == null) {
-                return null;
-            }
-            result.addAll(backtracking(map, new ArrayList<>(), map.units.get(CellType.Tortuga),
-                    map.units.get(CellType.DeadManChest), 0, false, false));
-            for (int i = 0; i < map.size; ++i) {
-                for (int j = 0; j < map.size; ++j) {
-                    map.cells[i][j].d = Integer.MAX_VALUE;
-                }
+        var result2 = backtracking(map, new ArrayList<>(), map.cells[0][0],
+                map.units.get(CellType.Tortuga), 0, false, false);
+        for (int i = 0; i < map.size; ++i) {
+            for (int j = 0; j < map.size; ++j) {
+                map.cells[i][j].d = Integer.MAX_VALUE;
             }
         }
-        result.add(0, map.cells[0][0]);
+        var result2part2 = backtracking(map, new ArrayList<>(), map.units.get(CellType.Tortuga),
+                map.units.get(CellType.DeadManChest), 0, false, false);
+        if (result2 != null && result2part2 != null) {
+            result2.addAll(result2part2);
+        } else {
+            if (result1 != null) result1.add(0, map.cells[0][0]);
+            return result1;
+        }
+        for (int i = 0; i < map.size; ++i) {
+            for (int j = 0; j < map.size; ++j) {
+                map.cells[i][j].d = Integer.MAX_VALUE;
+            }
+        }
+        if (result1 == null && result2 == null) return null;
+        if (result2 == null) {
+            result1.add(0, map.cells[0][0]);
+            return result1;
+        }
+        if (result1 == null) {
+            result2.add(0, map.cells[0][0]);
+            return result2;
+        }
+        if (result1.size() < result2.size()) {
+            result1.add(0, map.cells[0][0]);
+            return result1;
+        }
+        result2.add(0, map.cells[0][0]);
+        return result2;
+    }
+
+    private static void AStarAlgorithm(Map map, Cell start, Cell finish, boolean hasVisitedTortuga) {
+        Set<Cell> closed = new HashSet<>();
+        PriorityQueue<Cell> open = new PriorityQueue<>();
+        if (start.type == CellType.JackOnTortuga || start.type == CellType.Tortuga || hasVisitedTortuga) {
+            start.hasVisitedTortuga = true;
+        }
+        if (map.isKrakenWeakness(start) && start.hasVisitedTortuga) {
+            start.isKrakenKilled = true;
+        }
+        start.f = start.g = 0;
+        open.add(start);
+        while (!open.isEmpty() && !open.contains(finish)) {
+            Cell current = open.poll();
+            for (int i = Math.max(0, current.x - 1); i <= Math.min(map.size - 1, current.x + 1); ++i) {
+                for (int j = Math.max(0, current.y - 1); j <= Math.min(map.size - 1, current.y + 1); ++j) {
+                    if (i == current.x && j == current.y) continue;
+                    if (!closed.contains(map.cells[i][j]) && map.isNonDangerous(map.cells[i][j], current.isKrakenKilled)) {
+                        if (!open.contains(map.cells[i][j])) {
+                            if (map.cells[i][j].type == CellType.Tortuga || map.cells[i][j].type == CellType.JackOnTortuga || hasVisitedTortuga) {
+                                map.cells[i][j].hasVisitedTortuga = true;
+                            }
+                            if ((map.isKrakenWeakness(map.cells[i][j]) && map.cells[i][j].hasVisitedTortuga) || current.isKrakenKilled) {
+                                map.cells[i][j].isKrakenKilled = true;
+                            }
+                            map.cells[i][j].g = current.g + 1;
+                            map.cells[i][j].parent = current;
+                            map.cells[i][j].h = Math.max(Math.abs(map.units.get(CellType.DeadManChest).x - i),
+                                    Math.abs(map.units.get(CellType.DeadManChest).y - j));
+                            map.cells[i][j].f = map.cells[i][j].g + map.cells[i][j].f;
+                            open.offer(map.cells[i][j]);
+                        } else {
+                            if (current.g + 1 < map.cells[i][j].g) {
+                                map.cells[i][j].g = current.g + 1;
+                                map.cells[i][j].parent = current;
+                                map.cells[i][j].h = Math.max(Math.abs(map.units.get(CellType.DeadManChest).x - i),
+                                        Math.abs(map.units.get(CellType.DeadManChest).y - j));
+                                map.cells[i][j].f = map.cells[i][j].g + map.cells[i][j].f;
+                            }
+                        }
+
+                    }
+                }
+            }
+            closed.add(current);
+        }
+    }
+
+    private static ArrayList<Cell> AStarPath(Cell start, Cell finish) {
+        Cell current = finish;
+        ArrayList<Cell> result = new ArrayList<>();
+        result.add(current);
+        while (current != start) {
+            if (current == null) {
+                return null;
+            }
+            current = current.parent;
+            result.add(current);
+        }
+        result.remove(result.size() - 1);
+        Collections.reverse(result);
+        return result;
+    }
+
+    private static void clearAStarData(Map map) {
+        for (int i = 0; i < map.size; ++i) {
+            for (int j = 0; j < map.size; ++j) {
+                map.cells[i][j].parent = null;
+                map.cells[i][j].isKrakenKilled = false;
+                map.cells[i][j].hasVisitedTortuga = false;
+                map.cells[i][j].f = map.cells[i][j].g = map.cells[i][j].h = 0;
+            }
+        }
+    }
+
+    public static ArrayList<Cell> AStar(Map map) {
+        ArrayList<Cell> result1;
+        Cell jackSparrow = map.units.get(CellType.JackSparrow);
+        jackSparrow = map.cells[jackSparrow.x][jackSparrow.y];
+        Cell tortuga = map.units.get(CellType.Tortuga);
+        tortuga = map.cells[tortuga.x][tortuga.y];
+        Cell[] krakenWeaknesses = map.getKrakenWeaknesses();
+        Cell deadManChest = map.units.get(CellType.DeadManChest);
+        AStarAlgorithm(map, jackSparrow, deadManChest, false);
+        result1 = AStarPath(jackSparrow, deadManChest);
+        clearAStarData(map);
+
+        AStarAlgorithm(map, jackSparrow, tortuga, false);
+        var result2 = AStarPath(jackSparrow, tortuga);
+        if (result2 == null) {
+            if (result1 != null)
+                result1.add(0, map.cells[0][0]);
+            return result1;
+        }
+        clearAStarData(map);
+        ArrayList<ArrayList<Cell>> pathsKillingKraken = new ArrayList<>(4);
+        int minPathIndex = -1;
+        int minPathLength = Integer.MAX_VALUE;
+        for (int i = 0; i < krakenWeaknesses.length; ++i) {
+            AStarAlgorithm(map, tortuga, krakenWeaknesses[i], true);
+            pathsKillingKraken.add(AStarPath(tortuga, krakenWeaknesses[i]));
+            if (pathsKillingKraken.get(i) == null) {
+                continue;
+            }
+            clearAStarData(map);
+            AStarAlgorithm(map, krakenWeaknesses[i], deadManChest, true);
+            ArrayList<Cell> fromKrakenWeaknessToDeadManChest = AStarPath(krakenWeaknesses[i], deadManChest);
+            clearAStarData(map);
+            if (fromKrakenWeaknessToDeadManChest == null) {
+                pathsKillingKraken.set(i, null);
+                continue;
+            }
+            pathsKillingKraken.get(i).addAll(fromKrakenWeaknessToDeadManChest);
+            if (pathsKillingKraken.get(i) != null && pathsKillingKraken.get(i).size() < minPathLength) {
+                minPathLength = pathsKillingKraken.get(i).size();
+                minPathIndex = i;
+            }
+        }
+        if (minPathIndex < 0) result2 = null;
+        else result2.addAll(pathsKillingKraken.get(minPathIndex));
+        var result = result1;
+        if (result == null) result = result2;
+        else if (result2 != null && result1.size() > result2.size()) {
+            result = result2;
+        }
+        if (result != null)
+            result.add(0, map.cells[0][0]);
         return result;
     }
 }
--- src/Main2.java
+++ src/Main2.java
@@ -1,30 +1,19 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.PriorityQueue;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main2 {
-    public static boolean checkIfInputIsValid(String path) throws FileNotFoundException {
-        File input = new File(path);
-        Scanner in = new Scanner(input);
-        in.useDelimiter("\n");
-        String regEx = "^\\[\\d,\\d]\\s\\[\\d,\\d]\\s\\[\\d,\\d]\\s\\[\\d,\\d]\\s\\[\\d,\\d]\\s\\[\\d,\\d]";
-        String regEx2 = "[12]";
-        Pattern pattern = Pattern.compile(regEx);
-        if (!in.hasNext(pattern)) {
-            in.close();
-            return false;
+
+    public static void main(String[] args) {
+        PriorityQueue<Integer> q = new PriorityQueue<>();
+        q.offer(1);
+        q.offer(2);
+        q.offer(3);
+        while (!q.isEmpty()) {
+            System.out.println(q.poll());
         }
-        in.next();
-        if (!in.hasNext(Pattern.compile(regEx2))) {
-            in.close();
-            return false;
-        }
-        in.close();
-        return true;
-    }
-    public static void main(String[] args) throws FileNotFoundException {
-        checkIfInputIsValid("input.txt");
     }
 }
